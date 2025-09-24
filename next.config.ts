cf5d75944082c52f0ab6985a55a96f74aa7e39bd
Mehrosh-create <mehroshikram3@gmail.com>
2025-09-24 10:05:12 +0500

    fixed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com", // if you use source.unsplash.com links
+      },
+    ],
+  },
 };
 
 export default nextConfig;
