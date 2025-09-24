24933685d24356e5750697c1194b822cd3b74c89
Mehrosh-create <mehroshikram3@gmail.com>
2025-09-24 11:46:15 +0500

    font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,12 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
+const bebazNeue = Bebas_Neue({
+  weight: "400",
+  variable: "--font-bebas-neue",
+  subsets: ["latin"],
+});
+
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
@@ -28,7 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#151515] text-white overflow-x-hidden`}
+        className={`${geistSans.variable} ${geistMono.variable} ${bebazNeue.variable} antialiased bg-[#151515] text-white overflow-x-hidden`}
       >
         <ClientLayout>{children}</ClientLayout>
       </body>
